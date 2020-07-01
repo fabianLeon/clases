564537b8b937aee9b11ab37e84326794944c9ef6
fabian sanchez <fadarsaleeing@gmail.com>
2020-06-30 23:21:12 -0500

    map, reduce y filter

---
+++ clase8/js/catalogo-info.js
@@ -0,0 +1,31 @@
+const categorias = [
+    {
+        nombre: 'Nevera-Frutas'
+    }
+]
+const catalogo = [
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/millonarios.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/millonarios.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/millonarios.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+];
--- clase8/js/catalogo.js
+++ clase8/js/catalogo.js
@@ -1,47 +1,15 @@
-const categorias = [
-    {
-    }
-]
-const catalogo = [
-    {
-        categoria: 'Nevera-Frutas',
-        urlImagen: 'img/millonarios.jpg',
-        imagen: 'Porrista de millonarios',
-        titulo: 'Porrista de millonarios',
-        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
-        precio: '3000'
-    },
-    {
-        categoria: 'Nevera-Frutas',
-        urlImagen: 'img/millonarios.jpg',
-        imagen: 'Porrista de millonarios',
-        titulo: 'Porrista de santafe',
-        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
-        precio: '3000'
-    },
-    {
-        categoria: 'Nevera-apliques',
-        urlImagen: 'img/millonarios.jpg',
-        imagen: 'Porrista de millonarios',
-        titulo: 'Porrista de nacional',
-        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
-        precio: '3000'
-    },
-];
-
 const cargarCatalogo = (categoria) => {
-    
     const catalogoXCategorias = catalogo.filter((producto) => {
         return producto.categoria === categoria;
     });
 
     const catalogoHtml = catalogoXCategorias.map((producto) => {
         return `<div class="producto">
-                    <h2 class="titulo-producto">${producto.titulo}</h2>
-                    <img src="${producto.urlImagen}" alt="${producto.imagen}">
-                    <p class="descripcion">${producto.descripcion}</p>
-                </div>
-                <hr>`
+    <h2 class="titulo-producto">${producto.titulo}</h2>
+    <img src="${producto.urlImagen}" alt="${producto.imagen}">
+    <p class="descripcion">${producto.descripcion}</p>
+</div>
+<hr>`
     });
     let productosHtml = catalogoHtml.reduce((acumulado, siguiente) => {
         return (acumulado + siguiente);
