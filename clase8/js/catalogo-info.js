00ac4099d4750b98f49e2fc0e135e2525d42ac2d
leon <fabian leon>
2020-07-26 22:31:44 -0500

    catalogo con flexbox y maximo ancho

--- clase8/js/catalogo-info.js
+++ clase8/js/catalogo-info.js
@@ -52,4 +52,149 @@ const catalogo = [
         descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
         precio: '3000'
     },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/01.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/02.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/03.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/04.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/05.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/06.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/01.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/02.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/03.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/04.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/05.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/06.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/01.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/02.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/03.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-apliques',
+        urlImagen: 'img/04.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de nacional',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/05.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de millonarios',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
+    {
+        categoria: 'Nevera-Frutas',
+        urlImagen: 'img/06.jpg',
+        imagen: 'Porrista de millonarios',
+        titulo: 'Porrista de santafe',
+        descripcion: 'Esta es una linda fruta en forma de banano para decorar tu nevera',
+        precio: '3000'
+    },
 ];
+const mivariable = "Este mensaje";
--- clase8/js/catalogo.js
+++ clase8/js/catalogo.js
@@ -1,18 +1,32 @@
+
+console.log(mivariable);
+
 const cargarCatalogo = () => {
 
     const catalogoHtml = catalogo.map((producto) => {
         return `
 <div class="producto">
-    <img src="${producto.urlImagen}" alt="${producto.imagen}">
+    <img class="imagen-producto" src="${producto.urlImagen}" alt="${producto.imagen}">
     <h2 class="titulo-producto">${producto.titulo}</h2>
     <p class="descripcion">${producto.descripcion}</p>
-</div>
-<hr>`
+</div>`
     });
+
+    console.log(catalogo);
+    console.log(catalogoHtml);
     let productosHtml = catalogoHtml.reduce((acumulado, siguiente) => {
+        console.log(acumulado);
         return (acumulado + siguiente);
     });
+    console.log(productosHtml);
     document.getElementById('catalogo').innerHTML = productosHtml;
 }
 
 cargarCatalogo();
+
+const tablaDelDos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    .map((numero, index) => {
+        return (`2 X ${numero} = ${2 * numero}`)
+    })
+
+console.log(tablaDelDos);
