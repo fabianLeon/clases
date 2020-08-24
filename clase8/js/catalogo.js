48f8d162f3950ac4107acf58ea0fb6863860e40b
leon <fabian leon>
2020-08-23 19:48:14 -0500

    clase 9 y 10

--- clase8/js/catalogo.js
+++ clase8/js/catalogo.js
@@ -1,32 +1,38 @@
 
-console.log(mivariable);
 
 const cargarCatalogo = () => {
+    const catalogoHtml = catalogo.map(getProducto);
 
-    const catalogoHtml = catalogo.map((producto) => {
-        return `
-<div class="producto">
+    let productosHtml = catalogoHtml.reduce((acumulado, siguiente) => {
+        console.log('estas son categorias', categorias);
+        return (acumulado + siguiente);
+    });
+    document.getElementById('catalogo').innerHTML = productosHtml;
+    console.log(miVariable);
+}
+
+let miVariable = "Mi variable";
+
+const miarreglo = [1, 2, 3, 4, 5];
+let getProducto = (producto) => {
+    return `
+    <div class="producto">
     <img class="imagen-producto" src="${producto.urlImagen}" alt="${producto.imagen}">
     <h2 class="titulo-producto">${producto.titulo}</h2>
     <p class="descripcion">${producto.descripcion}</p>
-</div>`
-    });
-
-    console.log(catalogo);
-    console.log(catalogoHtml);
-    let productosHtml = catalogoHtml.reduce((acumulado, siguiente) => {
-        console.log(acumulado);
-        return (acumulado + siguiente);
-    });
-    console.log(productosHtml);
-    document.getElementById('catalogo').innerHTML = productosHtml;
+    </div>`
 }
 
 cargarCatalogo();
 
-const tablaDelDos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    .map((numero, index) => {
-        return (`2 X ${numero} = ${2 * numero}`)
-    })
+const tablaDelDos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((numero, index) => {
+    return (`2 X ${numero} = ${2 * numero}`);
+});
 
-console.log(tablaDelDos);
+if (true) {
+
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
